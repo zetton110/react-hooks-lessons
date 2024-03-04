4c3f1febe0de37bf092e30446a281b073ed50ed8
Shin-sibainu <shincode0712@gmail.com>
2024-03-04 09:30:44 +0900

    add lesson

--- src/lessons/Lesson1/Lesson1_1.tsx
+++ src/lessons/Lesson1/Lesson1_1.tsx
@@ -1,45 +1,9 @@
-import { useState } from "react";
-
-// const someFunction = () => {
-//   console.log("function!");
-// };
-
 const Lesson1_1 = () => {
-  const [age, setAge] = useState<number>(26);
-  const [name, setName] = useState<string>("ShinCode");
-  // const [todos, setTodos] = useState(someFunction);
-
-  console.log("rendered!");
-
-  const handleClick = () => {
-    // setAge(age + 1); //setAge(42 + 1)
-    // setAge(age + 1); //setAge(42 + 1)
-    // setAge(age + 1); //setAge(42 + 1)
-
-    setAge((prevAge) => prevAge + 1);
-    console.log(age);
-    setAge((prevAge) => prevAge + 1);
-    console.log(age);
-    setAge((prevAge) => prevAge + 1);
-    console.log(age);
-  };
-
   return (
     <div>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <button
-        className="border p-2 rounded-md bg-red-100"
-        onClick={handleClick}
-      >
-        Add Age
-      </button>
-      <p>
-        Hello, {name}. You are {age}
-      </p>
+      <input type="text" />
+      <button className="border p-2 rounded-md bg-red-100">Add Age</button>
+      <p></p>
     </div>
   );
 };
--- src/lessons/Lesson1/Lesson1_2.tsx
+++ src/lessons/Lesson1/Lesson1_2.tsx
@@ -1,69 +1,23 @@
-import { useState } from "react";
-//https://ja.react.dev/learn/updating-arrays-in-state#making-other-changes-to-an-array
-
-//object
 const Lesson1_2 = () => {
-  const [form, setForm] = useState({
-    firstName: "Code",
-    lastName: "Shin",
-    email: "shincode@gmail.com",
-  });
-
-  // form.firstName = "ModShin"; //✖mutation
-
   return (
     <div>
       <div className="flex mb-5">
         <label>
           First Name:
-          <input
-            value={form.firstName}
-            type="text"
-            onChange={(e) => {
-              //replace
-              setForm({
-                ...form,
-                firstName: e.target.value,
-              });
-            }}
-            className="border border-slate-500"
-          />
+          <input type="text" className="border border-slate-500" />
         </label>
         <label>
           Last Name:
-          <input
-            value={form.lastName}
-            type="text"
-            onChange={(e) => {
-              setForm({
-                ...form,
-                lastName: e.target.value,
-              });
-            }}
-            className="border border-slate-500"
-          />
+          <input type="text" className="border border-slate-500" />
         </label>
         <label>
           Email:
-          <input
-            value={form.email}
-            type="text"
-            onChange={(e) => {
-              setForm({
-                ...form,
-                email: e.target.value,
-              });
-            }}
-            className="border border-slate-500"
-          />
+          <input type="text" className="border border-slate-500" />
         </label>
       </div>
       <p>
-        {form.firstName}
         <br />
-        {form.lastName}
         <br />
-        {form.email}
       </p>
     </div>
   );
