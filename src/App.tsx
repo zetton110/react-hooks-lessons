2832e40d0180d1fab1b45b844922786db84105f6
zetton110 <locolv.kohe@gmail.com>
2025-01-20 23:48:09 +0900

    useState の１が終了

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-// import Lesson1_1 from "./lessons/Lesson1/Lesson1_1";
+import Lesson1_1 from "./lessons/Lesson1/Lesson1_1";
 
 function App() {
   return (
     <div className="flex items-center max-w-4xl mx-auto py-8 text-2xl">
-      {/* <Lesson1_1 /> */}
+      {<Lesson1_1 />}
       {/* <Lesson1_2 /> */}
       {/* <Lesson2_1 /> */}
       {/* <Lesson3_1 /> */}
--- src/lessons/Lesson1/Lesson1_1.tsx
+++ src/lessons/Lesson1/Lesson1_1.tsx
@@ -1,11 +1,39 @@
+import { useState } from "react";
+
 const Lesson1_1 = () => {
+
+  console.log("rendering!");
+
+  const [age, setAge] = useState<number>(0);
+  const handleClick = () => {
+    setAge((prevAge)=> prevAge + 1);
+  }
+
   return (
     <div>
       <input type="text" />
-      <button className="border p-2 rounded-md bg-red-100">Add Age</button>
-      <p></p>
+      <button 
+        onClick={handleClick}
+        className="border p-2 rounded-md bg-red-100">Add Age</button>
+      <p>You are {age}</p>
     </div>
   );
 };
 
 export default Lesson1_1;
+
+/**
+ * useState (https://ja.react.dev/reference/react/useState)
+ * - 用途
+ *   - 値の保存（メモ化）をする
+ * - 状態更新関数
+ *  - 前回の状態を引数に取る
+ *  - 状態更新関数が実行されるとコンポーネントは再レンダリングされる
+ *   - 子コンポーネントも再レンダリングされる
+ * - "rendering!" がコンソールに2回表示される
+ *  - React.StrictMode (https://ja.react.dev/reference/react/StrictMode)
+ *    - 開発モードでのみ有効(プロダクションでは無効になる)
+ *  - なぜか？
+ *    - コンポーネントを純粋に保つため (https://ja.react.dev/reference/rules/components-and-hooks-must-be-pure)
+ *    - 副作用を検出するため
+ */
