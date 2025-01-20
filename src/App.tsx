eead2bf89091681914ca5dcbc258d4a91358d227
zetton110 <locolv.kohe@gmail.com>
2025-01-21 01:17:09 +0900

    useStateの２が終了

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import Lesson1_1 from "./lessons/Lesson1/Lesson1_1";
+import Lesson1_2 from "./lessons/Lesson1/Lesson1_2";
 
 function App() {
   return (
     <div className="flex items-center max-w-4xl mx-auto py-8 text-2xl">
-      {<Lesson1_1 />}
-      {/* <Lesson1_2 /> */}
+      {/* {<Lesson1_1 />} */}
+      <Lesson1_2 />
       {/* <Lesson2_1 /> */}
       {/* <Lesson3_1 /> */}
       {/* ... */}
--- src/lessons/Lesson1/Lesson1_2.tsx
+++ src/lessons/Lesson1/Lesson1_2.tsx
@@ -1,26 +1,66 @@
+import { useState } from "react";
+
 const Lesson1_2 = () => {
+  const [form, setForm] = useState({
+    firstName: "aiu",
+    lastName: "eo",
+    email: "aiueo@gmail.com"
+  });
+
   return (
     <div>
       <div className="flex mb-5">
         <label>
           First Name:
-          <input type="text" className="border border-slate-500" />
+          <input 
+            type="text" 
+            className="border border-slate-500"
+            value={form.firstName}
+            onChange={(e) => setForm({
+              ...form,
+              firstName: e.target.value
+            })} />
         </label>
         <label>
           Last Name:
-          <input type="text" className="border border-slate-500" />
+          <input 
+            type="text" 
+            className="border border-slate-500"
+            value={form.lastName}
+            onChange={(e) => setForm({
+              ...form,
+              lastName: e.target.value
+            })} />
         </label>
         <label>
           Email:
-          <input type="text" className="border border-slate-500" />
+          <input 
+            type="text" 
+            className="border border-slate-500"
+            value={form.email}
+            onChange={(e) => setForm({
+              ...form,
+              email: e.target.value
+            })} />
         </label>
       </div>
       <p>
+        {form.firstName}
         <br />
+        {form.lastName}
         <br />
+        {form.email}
       </p>
     </div>
   );
 };
 
 export default Lesson1_2;
+
+/**
+ * useState (https://ja.react.dev/reference/react/useState)
+ * - スプレッド構文
+ *  - オブジェクトのプロパティを更新するときは、スプレッド構文を使って新しいオブジェクトを作成する
+ *  - https://ja.react.dev/learn/updating-objects-in-state
+ *   - 一度作成したデータは書き換えるべきでない
+ */
