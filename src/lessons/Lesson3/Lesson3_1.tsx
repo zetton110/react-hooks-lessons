c00b68105feaf410decc5992eac2977513aae351
zetton110 <locolv.kohe@gmail.com>
2025-01-21 13:44:00 +0900

    useRefの１と２が終了

--- src/lessons/Lesson3/Lesson3_1.tsx
+++ src/lessons/Lesson3/Lesson3_1.tsx
@@ -1,9 +1,22 @@
+import { useRef, useState } from "react";
+
 const Lesson3_1 = () => {
-  function handleClick() {}
+
+  const [input, setInput] = useState("");
+
+  const ref = useRef(0);
+  console.log(ref.current);
+
+  function handleClick() {
+    ref.current += 1;
+    console.log(ref.current);
+  }
 
   return (
     <div>
-      <input type="text" />
+      <input 
+        type="text"
+        onChange={(e) => setInput(e.target.value)} />
       <button onClick={handleClick}>Click me!</button>
       <p></p>
     </div>
--- src/lessons/Lesson3/Lesson3_2.tsx
+++ src/lessons/Lesson3/Lesson3_2.tsx
@@ -1,29 +1,43 @@
+import { RefObject, useRef } from "react";
+
 const Lesson3_2 = () => {
-  const scrollToIndex = () => {};
+  const listRef: RefObject<HTMLUListElement> = useRef<HTMLUListElement>(null);
+
+  const scrollToIndex = (index: number) => {
+    console.log(listRef.current);
+    const listNode = listRef.current;
+    const imgNode = listNode?.querySelectorAll("li > img")[index];
+    console.log(imgNode);
+
+    imgNode?.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
+    });
+  };
 
   return (
     <div>
       <nav>
-        <button onClick={() => scrollToIndex()}>Cat1</button>
-        <button onClick={() => scrollToIndex()}>Cat2</button>
-        <button onClick={() => scrollToIndex()}>Cat3</button>
+        <button onClick={() => scrollToIndex(0)}>Cat1</button>
+        <button onClick={() => scrollToIndex(1)}>Cat2</button>
+        <button onClick={() => scrollToIndex(2)}>Cat3</button>
       </nav>
       <div style={{ overflowX: "auto", maxWidth: "700px", margin: "auto" }}>
         <ul
           className="flex items-center justify-between"
           style={{ minWidth: "1300px" }} // コンテナより大きいサイズを指定
+          ref={listRef}
         >
-       <ul>
-  <li>
-    <img src="https://api.thecatapi.com/v1/images/search?size=small" alt="Cat 1" width="200" height="200" />
-  </li>
-  <li>
-    <img src="https://api.thecatapi.com/v1/images/search?size=med" alt="Cat 2" width="300" height="200" />
-  </li>
-  <li>
-    <img src="https://api.thecatapi.com/v1/images/search?size=small" alt="Cat 3" width="250" height="200" />
-  </li>
-</ul>
+          <li>
+            <img src="https://24.media.tumblr.com/tumblr_lzlke2FQIT1qgjltdo1_250.jpg" alt="Cat 1" width="200" height="200" />
+          </li>
+          <li>
+            <img src="https://cdn2.thecatapi.com/images/cf2.jpg" alt="Cat 2" width="300" height="200" />
+          </li>
+          <li>
+            <img src="https://26.media.tumblr.com/tumblr_m12o7iWoH21qzv52ko1_250.jpg" alt="Cat 3" width="250" height="200" />
+          </li>
         </ul>
       </div>
     </div>
